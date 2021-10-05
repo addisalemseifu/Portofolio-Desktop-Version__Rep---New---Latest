dbf791b4e772e6c429995368c34227c25efd1697
Addisalem Seifu <addisalemseifu@gmail.com>
2021-10-05 11:17:24 +0300

    Add code for the mobile menue

---
+++ app.js
@@ -0,0 +1,49 @@
+const menue = document.querySelector('.menue_bar');
+const menue_links= document.querySelector('.menue_bar_desktop');
+const navLogo = document.querySelector('#navbar__logo');
+
+
+menue.addEventListener('click', function(){
+    menue.classList.toggle('a');
+    menue_links.classList.toggle('active');
+});
+
+const menuBarHide = () => {
+    const menuBars = document.querySelector(".a");
+        if (window.innerWidth <= 960 && menuBars) {
+            menue.classList.toggle("a")
+            menue_links.classList.remove('active');
+        }
+    }
+
+    menue_links.addEventListener("click", menuBarHide);
+    menue_links.addEventListener("click", menuBarHide);
+
+
+
+
+
+    const modalTriggerButton = document.querySelector('.see-project');
+    const modal = document.querySelector('#popup');
+    const madalCloseButton = document.querySelector('.closeBtn');
+    
+    modalTriggerButton.addEventListener('click', event => {
+        modal.style.display = 'flex';
+        // document.body.style.overflow = 'hidden';
+    });
+    
+    madalCloseButton.addEventListener('click', event => {
+        modal.style.display = 'none';
+        // document.body.style.overflow = 'initial';
+    });
+    
+
+
+
+
+
+
+
+
+
+
