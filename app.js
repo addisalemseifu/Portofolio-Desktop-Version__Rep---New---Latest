dccecc60554a99c914e3e5247d4379f72f5aa342
Addisalem Seifu <addisalemseifu@gmail.com>
2021-10-08 13:35:46 +0300

    Modify concatination errors on line 34

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ const data = [{
 }];
 const output = document.querySelector('#portfolio');
 for (let x = 0; x < data.length; x += 1) {
-  output.innerHTML += "<div class='covers'><img class='img__class' src='/img/" + data[x].image + "'><div class='right__box'><h1 class='popup_tekst'>" + data[x].name + "<ul class='selections'><li class='main__txt__wrapper'><a href='#' class='main__txt' id='canopy_1'>CANOPY</a></li><li class='main__txt__wrapper'><a href='#' class='main__txt' id='canopy_2'>FACEBOOK</a></li><li class='circle__wrapper'><a href='#' class='circle'></a></li><li class='list__wrapper'><a href='#' class='list' id='back__1'>BACK END DEV</a></li><li class='list__wrapper'><a href='#' class='list' id='back__2'>Full Stack Dev</a></li><li class='circle__wrapper'><a href='#' class='circle'></a></li><li class='list__wrapper'><a href='#' class='list'>2015</a></li></ul></h1><p class='signup-disc'>" + data[x].description + "<ul class='platform'><li class='pla__btn'>html</li><li class='pla__btn'>css</li><li class='pla__btn'>javascript</li></ul></p><button onclick='openproject(" + x + ")' class='see-project'>see project</button></div></div>";
+  output.innerHTML = `${output.innerHTML}<div class='covers'><img class='img__class' src='/img/${data[x].image}'><div class='right__box'><h1 class='popup_tekst'>${data[x].name}<ul class='selections'><li class='main__txt__wrapper'><a href='#' class='main__txt' id='canopy_1'>CANOPY</a></li><li class='main__txt__wrapper'><a href='#' class='main__txt' id='canopy_2'>FACEBOOK</a></li><li class='circle__wrapper'><a href='#' class='circle'></a></li><li class='list__wrapper'><a href='#' class='list' id='back__1'>BACK END DEV</a></li><li class='list__wrapper'><a href='#' class='list' id='back__2'>Full Stack Dev</a></li><li class='circle__wrapper'><a href='#' class='circle'></a></li><li class='list__wrapper'><a href='#' class='list'>2015</a></li></ul></h1><p class='signup-disc'>${data[x].description}<ul class='platform'><li class='pla__btn'>html</li><li class='pla__btn'>css</li><li class='pla__btn'>javascript</li></ul></p><button onclick='openproject(${x})' class='see-project'>see project</button></div></div>`;
 }
 // const modalTriggerButton = document.querySelector('.see-project');
 const modal = document.querySelector('#popup');
 const madalCloseButton = document.querySelector('.closeBtn');
 function openproject(index) {
   document.querySelector('#popup_header').innerHTML = data[index].name;
-  document.getElementById('popupimg').src = '/img/' + data[index].image;
+  document.getElementById('popupimg').src = `/img/${data[index].image}`;
   modal.style.display = 'flex';
 }
 // openproject(0);
@@ -45,6 +45,7 @@ function openproject(index) {
 // });
 madalCloseButton.addEventListener('click', () => {
   modal.style.display = 'none';
+  openproject(null);
 });
 
 // const email = document.getElementsByClassName('text__input');
